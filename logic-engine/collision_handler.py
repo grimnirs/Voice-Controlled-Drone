8941bea1a67734d0911a222aca2fda89c1f12d89
Emmi Lidman <144234644+emli5303@users.noreply.github.com>
2026-04-20 15:55:24 +0200

    A start to a collision handler

---
+++ logic-engine/collision_handler.py
@@ -0,0 +1,18 @@
+from shared_variables import latest_dist, avoid_collision
+
+async def watch_distance(drone):
+    global latest_distance
+    print("Starting distance sensor...")
+    try:
+        async for distance in drone.telemetry.distance_sensor():
+            avoid_collision = False
+            print(f"SENSORDATA: Obstacle at: {latest_distance:.2f}m")
+
+            if distance.orientation == 0:
+                latest_distance = distance.current_distance_m
+
+                if latest_distance <= 1.0: 
+                    avoid_collision = True
+
+    except Exception as e:
+        print(f"Sensor-error: {e}")
